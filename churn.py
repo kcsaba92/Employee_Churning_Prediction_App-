6adff959974b3d5beb388e5fd8b9ce88c870fd4b
kcsaba92 <65985611+kcsaba92@users.noreply.github.com>
2020-11-12 13:07:44 -0800

    Update churn.py

--- churn.py
+++ churn.py
@@ -12,8 +12,6 @@
 
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import GradientBoostingClassifier
 from PIL import Image
 import streamlit as st
 import pickle
